d9efc945243c09b911ace62f164be34272d182d9
DanielPinto <danielsp.pinto@gmail.com>
2021-07-25 21:45:49 -0300

    Criação do projeto, criação do model Product e do Controller ProductController com os metodos create e index, e as rotas resource de Product - instalação dos pacotes Sanctum de autenticação, criação do AuthController com metodos : register, login e logout para geração de token e proteção das rotas com middleware.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Syscom
+
+Sistema de Comercio para fins de estudo na criação de uma API usando a Framework Laravel.
+### (Projeto em fase de desenvolvimento)
+
+## Skills 
+
+- **Área de Autenticação com geração de Token e proteção de rotas**
+- **Cadastro de Produtos**
+
+
+## License
+
+The Syscom é open-source para uso geral.
---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Product;
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+//Route::get('/products', 'ProductController@index');
+
+/*
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::resource('products', ProductController::class);    
+});
+*/
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('products', ProductController::class);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
